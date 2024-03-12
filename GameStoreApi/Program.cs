a29add69edf6b2e3fdff7ed065d3f94dce46ef0e
clemoz-dev <clemoz.dev@gmail.com>
2024-03-12 09:30:48 +0100

    games dto

--- GameStoreApi/Program.cs
+++ GameStoreApi/Program.cs
@@ -1,6 +1,34 @@
+using GameStoreApi.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<GameDto> games = [
+  new(
+    1,
+    "Street Fighter II",
+    "Fighting",
+    19.99M,
+    new DateOnly(1992,7,15)
+  ),  
+  new(
+    2,
+    "Final Fantasy XIV",
+    "Roleplaying",
+    59.99M,
+    new DateOnly(2010,9,30)
+  ),  
+  new(
+    3,
+    "FIFA 2023",
+    "Sports",
+    69.99M,
+    new DateOnly(2022,9,27)
+  ),  
+];
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/games", () => games);
 
 app.Run();
--- GameStoreApi/obj/Debug/net8.0/GameStoreApi.AssemblyInfo.cs
+++ GameStoreApi/obj/Debug/net8.0/GameStoreApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e18ed5c5da6e9a687477165049136c4ffde354c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
